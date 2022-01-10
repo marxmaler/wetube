8e11c9ea1e292d76902c2cddfa8ed656d71e6451
Sabi <84113149+marxmaler@users.noreply.github.com>
2022-01-11 03:34:34 +0900

    PORT env variable enabled

--- src/init.js
+++ src/init.js
@@ -6,8 +6,8 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000; //환경 변수에 PORT라는 변수 없으면 자동으로 4000 값이 들어감(즉, heroku가 아닌 경우 4000이 port값이 됨)
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
